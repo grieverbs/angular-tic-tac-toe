80e894dede125faf28de2f62ecce4426b3fa30cf
John Ho <ho.john.nguyen@gmail.com>
2024-02-28 21:50:33 -0700

    Using Material UI.

--- tic-tac-toe/src/app/game/game.component.ts
+++ tic-tac-toe/src/app/game/game.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { SquareComponent } from '../square/square.component';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [SquareComponent, CommonModule],
+  imports: [SquareComponent, CommonModule, MatGridListModule, MatButtonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.css'
 })
